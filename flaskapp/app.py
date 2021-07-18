9aa13f50f468dfcbbec92536c5d88df671d93c78
ephraim-amz <ephraimamezian@gmail.com>
2021-07-18 14:59:43 +0200

    Bug jquery fixé

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 data = pd.read_csv('dataset.csv')
 
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
@@ -13,9 +12,6 @@ def home():
 def load_random_movie():
     random_movie = data.iloc[randint(0, len(data)), :]
     return random_movie
-
-
-
 
 @app.route('/movie', methods=['GET'])
 def movie():
@@ -26,7 +22,6 @@ def movie():
     primary_name = random_movie['primaryName']
     genres = random_movie['genres']
     average_rating = random_movie['averageRating']
-    # elements = (title, director, primary_name, genres, average_rating)
     # Augmenter la variable numVotes lorsque l'utilisateur note un film
     return render_template('movie.html', title=title, director=director, primary_name=primary_name, genres=genres, average_rating=average_rating)
 
