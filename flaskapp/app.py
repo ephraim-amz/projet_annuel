1d24c5c66c6fa9c116ddce1c419441462dc32953
ephraim-amz <ephraimamezian@gmail.com>
2021-07-26 20:28:29 +0200

    Modif mineures app

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,13 +1,8 @@
 import pandas as pd
-import numpy as np
 from random import randint
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-data = pd.read_csv('categorical_dataset.csv')
-if 'user_note' not in data.columns:
-    data['user_note'] = None
-
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -46,23 +41,20 @@ def note():
 
     diff = float(format(float(diff), ".2f"))
     
-    
     return render_template('note.html', note_user=note_user, note_film=average_rating, title=title, director=director, primary_name=primary_name, genres=genres, annee_sortie=annee_sortie, hasNote=True, diff=diff)  
 
 @app.route('/no_note', methods=['GET','POST'])
 def note2():
     note_user = request.form['no_note']
-
     title = random_movie['originalTitle']
     director = random_movie['director']
     primary_name = random_movie['primaryName']
     genres = random_movie['genres']
     average_rating = random_movie['averageRating']
     annee_sortie = random_movie['startYear'] 
-
-
     return render_template('note.html', note_user=note_user, note_film=average_rating, title=title, director=director, primary_name=primary_name, genres=genres, annee_sortie=annee_sortie, hasNote=False)  
 
 
 if __name__ == '__main__':
+    data = pd.read_csv('categorical_dataset.csv')
     app.run(debug=True)
